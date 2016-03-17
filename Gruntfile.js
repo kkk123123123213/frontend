c097c9fe3fc3af016300a67053169c73ca6ce8a9
konrad.plazinski <konrad.plazinski@cybercom.com>
2016-03-17 10:43:30 +0100

    grunt clean + copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,76 @@
 module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cfg:
+        {
+            paths:
+            {
+                root: __dirname,
+                source: '<%= cfg.paths.root %>/src',
+                output: '<%= cfg.paths.root %>/dist',
+                bower: '<%= cfg.paths.root %>/bower_components'
+            }
+        },
+        connect: {
+            server: {   
+                options: {
+                    port: 9001,
+                    base: '<%= cfg.paths.source %>/',
+                    keepalive: false,
+                    hostname: '*',
+                    open: true,
+                    livereload: true
+                }
+            }
+        },
+        watch: {
+            index:
+            {
+                files: '<%= cfg.paths.source %>/*.html',
+                options: {
+                    hostname: '*',
+                    port: 9002,
+                    livereload: true
+                }
 
-  grunt.initConfig({
-	  pkg: grunt.file.readJSON('package.json'),
-	connect: {
-    server: {
-      options: {
-        port: 9001,
-        base: '',
-		keepalive:false,
-		hostname: '*',
-		open: true,
-		livereload: true
-      }
-    }
-  },
-    watch: {
-		index:
-		{
-		  files: ['*.html'],	
-			  options: {				 
-					hostname: '*',
-					port: 9002,
-					livereload:true				 
-			  }
-			
-		},
-    }
-  });
+            },
+        },
+        clean:
+        {
+            build:
+            {
+                src: '<%= cfg.paths.output%>/',
+                options:
+                {
+                    force: true
+                }
+            }
+        },
+        copy: {
+            html: {
+                src: '<%= cfg.paths.source %>/index.html',
+                dest: '<%= cfg.paths.output %>/index.html',
+            },
+            css: {
+                src: '<%= cfg.paths.source %>/jumbotron.css',
+                dest: '<%= cfg.paths.output %>/jumbotron.css',
+            },
+        },
+    });
 
-  require('load-grunt-tasks')(grunt, {
-    pattern: 'grunt-contrib-*',
-    config: 'package.json',
-    scope: 'devDependencies',
-    requireResolution: true
-});
 
-  grunt.registerTask('build', ['connect','watch']);
-  grunt.registerTask('default', ['build']);
-  grunt.registerTask('help', 'displays console help', function()
-  {
-	  grunt.log.writeln('Welcome...');
-  });
-  
+    require('load-grunt-tasks')(grunt, {
+        pattern: 'grunt-contrib-*',
+        config: 'package.json',
+        scope: 'devDependencies',
+        requireResolution: true
+    });
+
+    grunt.registerTask('build', ['clean', 'copy', 'connect', 'watch']);
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('help', 'displays console help', function() {
+        grunt.log.writeln('Welcome...');
+    });
+
 
 };
